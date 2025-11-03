889dfe01fdf6eb5646cdb04777e4852bbbef6127
cwestg20 <chris@cwestgate.com>
2025-11-02 19:41:26 -0500

    styling fixes

--- games/fruit-lines/nav.js
+++ games/fruit-lines/nav.js
@@ -3,7 +3,6 @@ const navHTML = `
 <nav>
     <div class="nav-content">
         <a href="../../index.html" class="logo">
-            <img src="../../intense.png" alt="Overtiger Logo" class="logo-image">
             <span>Overtiger</span>
         </a>
         <ul class="nav-links">
